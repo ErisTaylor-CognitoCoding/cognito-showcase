45f0dc96c203f9798d55f51049c2906c81939dd9
Eris Taylor <ErisTaylor@CognitoCoding.com>
2026-06-25 13:52:06 +0100

    feat: add sunflowers solution — The Farmer Was Replaced

---
+++ projects/the-farmer-was-replaced/sunflowers.py
@@ -0,0 +1,42 @@
+def sunflowers():
+
+	while get_pos_x() != 0:
+		move(East)
+	while get_pos_y() != 0:
+		move(North)
+
+	#Pass 1 - Clear the grass till land and plant
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if get_ground_type() != Grounds.Soil:
+				harvest()
+				till()
+				plant(Entities.Sunflower)
+				change_hat(Hats.Traffic_Cone)
+			if get_entity_type() == None:
+				plant(Entities.Sunflower)
+			move(North)
+		move(East)
+
+
+	#Pass 2 - water and scan petals in one sweep
+	max_petals = 0
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if get_entity_type() != None:
+				while get_water() < 0.75 and num_items(Items.Water) > 0:
+					use_item(Items.Water)
+			p = measure()
+			if p !=None and p > max_petals:
+				max_petals = p
+			move(North)
+		move(East)
+
+	#Pass 3 - Harvest the sunflower with the most petals and replant
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if measure() ==max_petals:
+				harvest()
+			plant(Entities.Sunflower)
+			move(North)
+		move(East)
